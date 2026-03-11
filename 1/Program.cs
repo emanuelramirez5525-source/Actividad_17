1e98f0efbd1efdccc8a582fc885d91bcb41a8019
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 11:31:56 -0600

    Programa Creado

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//Crear una función que reciba dos números y devuelva la suma de ambos.
+//Crear una función que reciba dos números y devuelva la suma de ambos
