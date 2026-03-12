82412cfc80c8d9e944424997117bf0d961eeb302
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 20:08:40 -0600

    Logica Aplicada!

--- 6/Program.cs
+++ 6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crear una función que reciba un número en grados Celsius y devuelva el valor convertido a Fahrenheit.
+
+double ConvertirCelsiusFarenheit(double celsius)
+{
+   double resultado = (celsius * 9 / 5) + 32;
+    return resultado;
+}
+
+Console.WriteLine("Ingrese los grados Celsius");
+double grados = double.Parse(Console.ReadLine());
+
+double resultado = ConvertirCelsiusFarenheit(grados);
+Console.WriteLine("El resultado es = " + resultado + " Farenheit");
