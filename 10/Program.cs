1ae640ca4454055975a6280d9c1a65bfdb75f251
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 21:26:57 -0600

    Logica Aplicada!

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una función que reciba un número y devuelva el cubo del número.
+
+int Cubo(int a)
+{
+    return a * a * a;
+}
+
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+int resultado = Cubo(num);
+Console.WriteLine("El cubo es = "+resultado);
