975aa1e91c05b1b1e21df90d6dcc3beeeaf26de4
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 20:41:04 -0600

    Logica Aplicada

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crear una función que reciba tres números y devuelva el promedio de los tres.
+
+double Promedio (double a, double b, double c)
+{
+    double PromedioTotal = (a + b + c) / 3;
+    return PromedioTotal;
+}
+
+Console.WriteLine("Ingrese el primer número");
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número");
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el tercer número");
+double num3 = double.Parse(Console.ReadLine());
+
+double PromedioTotal = Promedio(num1, num2, num3);
+
+Console.WriteLine("El promedio de los 3 números es = " + PromedioTotal);
