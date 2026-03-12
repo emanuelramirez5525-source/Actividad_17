ac29e6c4e69c61480ce5afa0bab0c7a6c41858ec
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 20:24:13 -0600

    Logica Aplicada!

--- 7/Program.cs
+++ 7/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una función que reciba el precio de un producto y el porcentaje de descuento y devuelva el precio final.
+
+double PrecioFinal(double precio, double descuento)
+{
+    double PrecioFinal = precio - (precio * descuento / 100);
+    return PrecioFinal;
+}
+
+Console.WriteLine("Ingrese el precio");
+double precio = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad del descuento");
+double descuento = double.Parse(Console.ReadLine());
+
+Double FinalPrecio = PrecioFinal(precio, descuento);
+Console.WriteLine("El precio final es = "+FinalPrecio);
