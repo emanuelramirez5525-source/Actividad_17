be9067b4672636aaad841c88f0de25307f2e7aed
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 21:56:50 -0600

    Logica Aplicada!

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una función que reciba el peso en kilogramos y devuelva el peso en libras.
+
+double KilogramosLibras (double Kilogramos)
+{
+    return Kilogramos * 2.20462;
+}
+
+Console.WriteLine("Ingrese los kilogramos");
+double kg = double.Parse(Console.ReadLine());
+
+double libras = KilogramosLibras(kg);
+
+Console.WriteLine("El peso en libras es = "+libras);
