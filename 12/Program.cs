9f40e8d54eadcdac663a8353c61faf48876f60d2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 22:33:37 -0600

    Logica Aplicada!

--- 12/Program.cs
+++ 12/Program.cs
@@ -1,2 +1,12 @@
 ﻿//Crear una función que reciba la edad de una persona y devuelva un mensaje mostrando la edad ingresada.
 
+string MostrarEdad(int edad)
+{
+    return "la edad de la persona es " + edad;
+}
+
+Console.WriteLine("Ingrese una edad ");
+int edad = int.Parse(Console.ReadLine());
+
+string mostrar = MostrarEdad(edad);
+Console.WriteLine(mostrar);
