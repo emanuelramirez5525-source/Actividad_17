f2100699c0db343b6534e6bae76e18822db364bb
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 23:36:13 -0600

    Logica Aplicada!

--- 15/Program.cs
+++ 15/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Crear una función que reciba la base y la altura de un triángulo y devuelva su área
 
+double Area(int Base, int Altura)
+{
+    return (Base * Altura) / 2;
+}
+Console.WriteLine("Ingrese la base");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura");
+int n2 = int.Parse(Console.ReadLine());
+
+double area = Area(n1, n2);
+Console.WriteLine("La área es = " + area);
