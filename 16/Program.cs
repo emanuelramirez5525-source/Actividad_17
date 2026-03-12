cee5f80fa44391eb3de700bc43407a4e708f9c84
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 00:03:08 -0600

    Logica Aplicada!

--- 16/Program.cs
+++ 16/Program.cs
@@ -1,1 +1,13 @@
 ﻿//Crear una función que reciba una cantidad de minutos y devuelva el equivalente en horas.
+
+double MinutosHoras (double Minutos)
+{
+    double resultado = (Minutos / 60);
+    return resultado;
+}
+
+Console.WriteLine("Ingrese una cantidad de minutos");
+double minuto = double.Parse(Console.ReadLine());
+
+double equivalencia = MinutosHoras(minuto);
+Console.WriteLine("La equivalencia de minutos a horas es = "+ equivalencia+ "hora(s)");
