26692160d9c3b454db7bb3567177ae92b04ea9d1
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 19:09:13 -0600

    Logica Aplicada!

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Crear una función que reciba dos números y devuelva la suma de ambos
+int Sumar(int a, int b)
+{
+    return a + b;
+}
+
+int resultado = Sumar(5, 10);
+Console.WriteLine(resultado);
