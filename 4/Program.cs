5b1a8e018f4cba091220343013912bf967741fcf
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 19:41:08 -0600

    Logica Aplicada!

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una función que reciba el radio de un círculo y devuelva el área del círculo.
+
+double AreaCirculo(int radio)
+{
+    return Math.PI * radio * radio;
+}
+
+Console.WriteLine("Ingrese el circulo");
+int num = int.Parse(Console.ReadLine());
+
+double area = AreaCirculo(num);
+
+Console.WriteLine("El area de un circulo = "+ area);
