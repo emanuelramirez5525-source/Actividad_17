cab068496d327073569fa118b785a30b373a142e
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 19:54:40 -0600

    Logica Aplicada!

--- 5/Program.cs
+++ 5/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear una función que reciba la base y la altura de un rectángulo y devuelva su área.
+
+Double Area(int Base, int Altura)
+{
+    return Base * Altura;
+}
+Console.WriteLine("Ingrese la base");
+int n1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura");
+int n2 = int.Parse(Console.ReadLine());
+
+double area = Area(n1, n2);
+Console.WriteLine("La área es = "+ area);
