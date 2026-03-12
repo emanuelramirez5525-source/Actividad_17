00d0d1f91c3963ddfe2d883bff5e6782aa4ab7dd
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 19:21:04 -0600

    Logica Aplicada!

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,1 +1,13 @@
 ﻿//Crear una función que reciba un número y devuelva su cuadrado
+
+
+int Cuadrado(int a)
+{
+    return a * a;
+}
+
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+int resultado = Cuadrado(num);
+Console.WriteLine("El cuadrado es = "+ resultado);
