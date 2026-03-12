3cee29afd756426f5df31d5731aa84e3858c61c2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-11 19:25:35 -0600

    Logica Aplicada!

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,14 @@
-﻿// 
+﻿// Crear una función que reciba un número y devuelva su doble.
+
+int Doble(int a)
+{
+    return a * 2;
+}
+
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+
+int resultado = Doble(num);
+
+Console.WriteLine("El doble es = "+ resultado);
