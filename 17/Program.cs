4b5b19aaaaab8ae514bc25625cee0b60d86dc663
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 09:38:36 -0600

    Logica Aplicada!

--- 17/Program.cs
+++ 17/Program.cs
@@ -1,3 +1,20 @@
 ﻿//Crear una función que reciba la cantidad de estudiantes y la nota de cada uno y devuelva el promedio del grupo.
 
+double Promedio(double cantidadestudiante)
+{
+    double suma = 0;
+    for (int i = 1; i <=cantidadestudiante; i++)
+    {
+        Console.WriteLine("Ingrese la nota de los estudiantes");
+        double nota = double.Parse(Console.ReadLine());
+        suma = suma + nota;
+    }
+    double promedio = suma / cantidadestudiante;
+    return promedio;
+}
 
+Console.WriteLine("Ingrese la cantidad de estudiantes");
+int cantidadestudiante = int.Parse(Console.ReadLine());
+
+double PromedioTotal = Promedio(cantidadestudiante);
+Console.WriteLine("EL Promedio total del grupo es = " + PromedioTotal);
