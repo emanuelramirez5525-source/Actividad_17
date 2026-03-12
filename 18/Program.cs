4da7841dcb4f4c3ba2b866dd199f955927221424
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 09:51:13 -0600

    Logica Aplicada!

--- 18/Program.cs
+++ 18/Program.cs
@@ -1,3 +1,25 @@
 ﻿// Crear una función que reciba la cantidad de productos comprados y el precio de cada uno y devuelva el total de la factura.
 
 
+double TotalFactura(double cantidadproductos)
+{
+    double suma = 0;
+    for (int i = 1; i <= cantidadproductos; i++)
+    {
+        Console.WriteLine("Ingrese el precio de los productos comprados" + i);
+        double producto = double.Parse(Console.ReadLine());
+        suma = suma + producto; 
+    }
+    
+    return suma;
+
+    
+}
+
+Console.WriteLine("Ingrese la cantidad de productos comprados");
+int productos = int.Parse(Console.ReadLine());
+
+double total = TotalFactura(productos);
+Console.WriteLine("El total de la fractura es = "+ total);
+
+
