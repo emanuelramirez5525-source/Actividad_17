34f5164517d560c0715ef1541b2b3c426c3eff75
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 21:16:47 -0600

    Logica Aplicada!

--- 26/Program.cs
+++ 26/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Crear un procedimiento que solicite la edad de una persona y muestre el año aproximado de nacimiento.
 
+void AñoAproximado(int Edad)
+{
+    int anioActual = 2026;
+    int nacimiento = anioActual - Edad;
+
+    Console.WriteLine("La año aproximado es = "+ nacimiento);
+
+}
+
+Console.WriteLine("Ingrese la edad de la persona");
+int edad = int.Parse(Console.ReadLine());
+
+AñoAproximado(edad);
+
