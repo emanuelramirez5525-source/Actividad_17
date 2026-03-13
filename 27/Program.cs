bc58508716ba9c14098e457cab50e864a0f299e2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 21:26:26 -0600

    Logica Aplicada!

--- 27/Program.cs
+++ 27/Program.cs
@@ -1,3 +1,12 @@
 ﻿// Crear un procedimiento que solicite el precio de un producto y muestre el precio con un descuento del 10%.
 
- 
+void Descuento(double precio)
+{
+    double descuento = precio - (precio * 0.10);
+    Console.WriteLine("El precio es = " + precio + " y con el descuento del 10% es = " + descuento);
+}
+
+Console.WriteLine("Ingrese el precio de un producto");
+double producto = double.Parse(Console.ReadLine());
+
+Descuento(producto);
