7918943d1a653653338ce56c4e7386784ab5be27
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 21:49:50 -0600

    Logica Aplicada!

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Crear un procedimiento que solicite el número de estudiantes de un curso y muestre un mensaje indicando cuántos estudiantes hay en la clase.
+
+void EstudiantesClases(int Cantidad)
+{
+    Console.WriteLine("El total de estudiantes que hay en la clase es = "+Cantidad);
+}
+
+Console.WriteLine("Ingrese el número de estudiantes de un curso");
+int estudiantes = int.Parse(Console.ReadLine());
+
+EstudiantesClases(estudiantes);
