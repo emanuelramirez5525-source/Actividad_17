a4155bd3b7ae7afdf1bdf39e0d8186b00e9bc8e2
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 20:48:37 -0600

    Logica Aplicada!

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,1 +1,15 @@
 ﻿// Crear un procedimiento que solicite la base y la altura de un rectángulo y muestre su área.
+
+void Area(double BaseRectangulo, double altura)
+{
+    double area = BaseRectangulo * altura;
+    Console.WriteLine("El area de la base y altura de un rectangulo es = "+ area);
+}
+
+Console.WriteLine("Ingrese la base");
+double BaseRect = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectangulo");
+double altura = double.Parse(Console.ReadLine());
+
+Area(BaseRect, altura);
