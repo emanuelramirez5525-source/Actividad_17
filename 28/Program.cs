0f737329d2ec1c98621981e5efaacd1d80866979
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 21:33:46 -0600

    Programa Creado!

--- 28/Program.cs
+++ 28/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Crear un procedimiento que solicite tres calificaciones y muestre el promedio del estudiante.
