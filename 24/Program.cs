f725fbd82e5011ce24cdc2224f67fe188a79b8a8
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 19:38:53 -0600

    Programa Creado!

--- 24/Program.cs
+++ 24/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear un procedimiento que solicite el radio de un círculo y muestre el área del círculo.
