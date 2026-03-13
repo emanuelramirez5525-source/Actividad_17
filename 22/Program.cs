0765a805f9d78e18ab01ac7f2ee0a3bee5000879
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 19:33:30 -0600

    Logica Aplicada!

--- 22/Program.cs
+++ 22/Program.cs
@@ -1,2 +1,12 @@
 ﻿// Crear un procedimiento que solicite el nombre de una persona y muestre un saludo personalizado.
 
+void SaludoPersonalizado(string nombre)
+{
+    Console.WriteLine("Hola " + nombre + " Como estas?? =D");
+}
+
+Console.WriteLine("Ingrese el nombre de una persona");
+string nombre = Console.ReadLine();
+
+SaludoPersonalizado(nombre);
+
