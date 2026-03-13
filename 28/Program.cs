a823361973009a4a05f998d68ff27f978be2f4fd
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 21:42:39 -0600

    Logica Aplicada!

--- 28/Program.cs
+++ 28/Program.cs
@@ -1,1 +1,17 @@
 ﻿//Crear un procedimiento que solicite tres calificaciones y muestre el promedio del estudiante.
+
+void Promedio(double a, double b, double c)
+{
+    double total = (a+ b + c) / 3;
+    Console.WriteLine("El promedio total ingresado es = "+total);
+}
+Console.WriteLine("Ingrese la primera calificacion");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la primera calificacion");
+double nota2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la primera calificacion");
+double nota3 = double.Parse(Console.ReadLine());
+
+Promedio(nota1, nota2, nota3);  
