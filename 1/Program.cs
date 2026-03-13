01e289454613675298e5a5f508c19f8e8c025f75
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 19:55:45 -0600

    Logica Aplicada!

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 //Crear una función que reciba dos números y devuelva la suma de ambos
 int Sumar(int a, int b)
 {
--- 24/Program.cs
+++ 24/Program.cs
@@ -1,1 +1,12 @@
 ﻿// Crear un procedimiento que solicite el radio de un círculo y muestre el área del círculo.
+
+void AreaCirculo(double radio)
+{
+    double total = Math.PI * (radio * radio);
+    Console.WriteLine("El area de un circulo es = "+total);
+}
+
+Console.WriteLine("Ingrese el radio de un circulo");
+double radio = double.Parse(Console.ReadLine());
+
+AreaCirculo(radio);
