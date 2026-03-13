2d1d95f052f1e8b1b7c544e1049ac1b3d961dfe5
emanu <emanu@EMANUEL_RAMREZ>
2026-03-12 19:38:27 -0600

    Logica Aplicada!

--- 23/Program.cs
+++ 23/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Crear un procedimiento que solicite dos números al usuario y muestre la suma de ambos.
+
+void Suma(int a, int b)
+{
+    int total = a + b;
+    Console.WriteLine("El resultado es = " + total);
+}
+
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese un segundo número");
+int num2 = int.Parse(Console.ReadLine());
+
+Suma(num, num2);
